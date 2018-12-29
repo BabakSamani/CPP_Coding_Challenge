bbf559052be7c6498252b863e66323554af18261
babaksmani <Babak.j.samani@gmail.com>
2018-12-29 15:22:11 -0600

    Updates on car.hpp file

--- Vehicle/car.hpp
+++ Vehicle/car.hpp
@@ -23,10 +23,31 @@ private:
 	std::string model;
 	std::string color;
 
+	double price;
+
 public:
+	// CClass constructor and destructor
 	car();
 	car(std::string VIN, std::string made, std::string year, std::string model, std::string color);
 	~car();
+
+	// Class setters
+	void setVIN(std::string _VIN);
+	void setMade(std::string _made);
+	void setYear(std::string _year);
+	void setModel(std::string _model);
+	void setColor(std::string _color);
+	void setPrice(double _price);
+
+	// Class getters
+	std::string getVIN() const;
+	std::string getMade() const;
+	std::string getYear() const;
+	std::string getModel() const;
+	std::string getColor() const;
+	double getPrice() const;
+
+
 	
 };
 
